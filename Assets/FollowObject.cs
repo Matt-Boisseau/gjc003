5e085cde6bb05fa7fa6e0527adcfcca7ac495225
Matt <example@email.com>
2019-01-16 21:21:07 -0500

    Add basic camera tracking

---
+++ Assets/FollowObject.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FollowObject : MonoBehaviour {
+    
+	public Transform objectToFollow;
+
+	private void Update() {
+		Vector3 targetPosition = new Vector3(objectToFollow.position.x, objectToFollow.position.y, 0);
+		Vector3 cameraPosition = new Vector3(this.transform.position.x, this.transform.position.y, 0);
+		transform.position += (targetPosition - cameraPosition) * Time.deltaTime;
+	}
+}
