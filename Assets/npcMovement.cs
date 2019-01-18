82de58f0f946d75e60708dcec64ef23f0938a444
tsmyers1019 <tsmyers1019@gmail.com>
2019-01-18 14:34:43 -0500

    npc movement basics
    added animations, controllers, and basic player controlled movement to
    an npc object

---
+++ Assets/npcMovement.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class npcMovement : MonoBehaviour
+{
+    public float speed;
+    private Animator animator;
+    public Vector2 moveDirection;
+ 
+    // Use this for initialization
+    void Start()
+    {
+        animator = this.GetComponent<Animator>();
+    }
+ 
+    // Update is called once per frame
+    void Update()
+    {
+       animator.SetInteger("walkInt", 0); // drop to neutral 
+
+        moveDirection = new Vector2(0,0);
+        var vertical = Input.GetAxis("Vertical");
+        var horizontal = Input.GetAxis("Horizontal");
+     
+        if (horizontal > 0)
+        {
+            moveDirection.x = 1;
+            animator.SetInteger("walkInt", 1);
+            //animator.SetInteger("Direction", 2);
+        }
+        else if (horizontal < 0)
+        {
+            moveDirection.x = -1;
+            animator.SetInteger("walkInt", -1);
+            //animator.SetInteger("Direction", 0);
+        }
+        else if (vertical > 0)
+        {
+            moveDirection.y = 1;
+            animator.SetInteger("walkInt", 2);
+            //animator.SetInteger("Direction", 1);
+        }
+        else if (vertical < 0)
+        {
+            moveDirection.y = -1;
+            animator.SetInteger("walkInt", -2);
+            //animator.SetInteger("Direction", 3);
+        }
+        transform.Translate(moveDirection * speed* Time.deltaTime, Space.World);
+    }
+}
